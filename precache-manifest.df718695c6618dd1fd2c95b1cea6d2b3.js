05ae1a09aa68c8ea58e64dafcb8532bdf5d1c644
SPOORTI <kspoorti08@gmail.com>
2020-08-14 14:10:38 +0530

    Updates

---
+++ precache-manifest.df718695c6618dd1fd2c95b1cea6d2b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1042a07f3405f53b29e1e4b2c75d0a",
+    "url": "/counterApp-react/index.html"
+  },
+  {
+    "revision": "af9aee4ed62d13c6d724",
+    "url": "/counterApp-react/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "af9aee4ed62d13c6d724",
+    "url": "/counterApp-react/static/js/2.71580598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counterApp-react/static/js/2.71580598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df147dd806236e82bce",
+    "url": "/counterApp-react/static/js/main.f98e57a7.chunk.js"
+  },
+  {
+    "revision": "9fa9dc3740744443acd7",
+    "url": "/counterApp-react/static/js/runtime-main.a37f19a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/counterApp-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
